21dede40c8918aa7ee868a87cde02e6ff9637c86
Nicolassts <161380754+Nicolassts@users.noreply.github.com>
2024-03-20 15:43:42 -0300

    Add files via upload

---
+++ pizzas.js
@@ -0,0 +1,98 @@
+pizzaJson=[
+    {
+      id: 1,
+      name: 'Mussarela',
+      img: 'images/mussarela 1.jpg',
+      price: [20.00, 23.00, 25.00],
+      sizes: [
+        '6 fatias',
+        '8 fatias',
+        '12 fatias'
+      ],
+      description: 'Molho de tomate, camada dupla de mussarela e orégano'
+    },
+    {
+      id: 2,
+      name: 'Calabresa',
+      img: 'images/calabresa 2.jpg',
+      price: [21.00, 24.00, 26.00],
+      sizes: [
+        '6 fatias',
+        '8 fatias',
+        '12 fatias'
+      ],
+      description: 'Molho de tomate, mussarela, calabresa fatiada, cebola e orégano'
+    },
+    {
+      id: 3,
+      name: 'Quatro Queijos',
+      img: 'images/quatro queijo 3.jpg',
+      price: [20.00, 23.00, 25.00],
+      sizes: [
+        '6 fatias',
+        '8 fatias',
+        '12 fatias'
+      ],
+      description: 'Molho de tomate, camadas de mussarela, provolone, parmessão, gorgonzola e orégano'
+    },
+    {
+      id: 4,
+      name: 'Brasileira',
+      img: 'images/brasileira.jpg',
+      price: [20.00, 23.00, 25.00],
+      sizes: [
+        '6 fatias',
+        '8 fatias',
+        '12 fatias'
+      ],
+      description: 'Molho de tomate, mussarela, calabresa picada, palmito, champignon, azeitonas e orégano'
+    },
+    {
+      id: 5,
+      name: 'Portuguesa',
+      img: 'images/portuguesa.jpg',
+      price: [20.00, 23.00, 25.00],
+      sizes: [
+        '6 fatias',
+        '8 fatias',
+        '12 fatias'
+      ],
+      description: 'Molho de tomate, mussarela, presunto, ovos, cebolas, pimentão, azeitona  e orégano'
+    },
+    {
+      id: 6,
+      name: 'Moda da Casa',
+      img: 'images/da casa.jpg',
+      price: [20.00, 23.00, 25.00],
+      sizes: [
+        '6 fatias',
+        '8 fatias',
+        '12 fatias'
+      ],
+      description: 'Molho de tomate, mussarela, carne de sol, tomates em cubos, coentro, cebola, azeitona, catupiry e orégano'
+    },
+    {
+      id: 7,
+      name: 'Banana com canela',
+      img: 'images/banana.jpg',
+      price: [20.00, 23.00, 25.00],
+      sizes: [
+        '6 fatias',
+        '8 fatias',
+        '12 fatias'
+      ],
+      description: 'Mussarela, banana, canela e açúcar'
+    },
+    {
+      id: 8,
+      name: 'Chocolate com morango',
+      img: 'images/morango.jpg',
+      price: [20.00, 23.00, 25.00],
+      sizes: [
+        '6 fatias',
+        '8 fatias',
+        '12 fatias'
+      ],
+      description: 'Creme de leite, lascas de chocolate e morangos'
+    }
+  ];
